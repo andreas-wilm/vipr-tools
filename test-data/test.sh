fee1064e403618ecb81e9dba26a9fb2e640daa3e
Andreas WILM <wilma@gis.a-star.edu.sg>
2015-12-04 14:57:31 +0800

    error messages in testing now more useful

--- test-data/test.sh
+++ test-data/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # http://www.redsymbol.net/articles/unofficial-bash-strict-mode/
-set -euo pipefail
+#set -euo pipefail
 IFS=$'\n\t'
 
 
@@ -14,16 +14,25 @@ log=$(mktemp -t test.XXXXXX)
 echo "Will exit 1 at any stage if tests fail" | tee -a $log
 echo "See $log for more info" | tee -a $log
 for ref in DENV_1.fa HIV_1.fa; do
+    randfa="rand.fa"
+    outfa="out.fa"
     echo "$NUM_TESTS times generating random contigs from $ref and stitching them back together." | tee -a $log
     for i in $($seq 1 $NUM_TESTS); do
-        python ./create_contigs.py $ref > rand.fa;
-        $JOINER -c rand.fa -r $ref -o joiner_out -v -v > out.fa 2>>$log;
-        for f in $ref out.fa; do
-            grep -v '^>' $f | tr -d '\n'  | $md5;
-        done  | sort -u | wc -l | grep -cq 1  || exit 1; # should only output one md5
+        python ./create_contigs.py $ref > $randfa;
+        rm $outfa 2>/dev/null
+        cmd="$JOINER -c $randfa -r $ref -o $outfa -v -v"
+        #echo "DEBUG Executing $cmd"
+        eval $cmd 2>>$log;
+        # should only output one md5
+        num_sqs=$(for f in $ref $outfa; do grep -v '^>' $f | tr -d '\n'  | $md5; done | sort -u | wc -l)
+        if [ $(echo $num_sqs) -ne 1 ]; then
+            echo "ERROR: seq in $outfa and $ref differ" 1>&2
+            echo "ERROR: command was $cmd" 1>&2
+            exit 1
+        fi
         echo "$ref: $i/$NUM_TESTS passed" | tee -a $log
     done
 done
-echo "All tests passed"
+echo "Done: all tests passed"
 #rm $log
 
