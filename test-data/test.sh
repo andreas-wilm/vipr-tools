497307794892b6fc4c037595e1ee6d772e6fc2c5
Andreas WILM <wilma@gis.a-star.edu.sg>
2015-12-04 15:12:58 +0800

    reenabled stict bash mode

--- test-data/test.sh
+++ test-data/test.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 # http://www.redsymbol.net/articles/unofficial-bash-strict-mode/
-#set -euo pipefail
+set -euo pipefail
 IFS=$'\n\t'
 
+NUM_TESTS=10
+JOINER=../simple_contig_joiner.py
 
 md5=$(which md5sum 2>/dev/null || which md5)
 seq=$(which seq 2>/dev/null || which gseq)
-NUM_TESTS=10
-JOINER=../simple_contig_joiner.py
 log=$(mktemp -t test.XXXXXX)
 
-echo "Will exit 1 at any stage if tests fail" | tee -a $log
+SUCCESS_MSG="Done: all tests passed"
+echo "Test only successful if final message says: $SUCCESS_MSG" | tee -a $log
+echo "Might exit 1 at any stage on failure" | tee -a $log
 echo "See $log for more info" | tee -a $log
 for ref in DENV_1.fa HIV_1.fa; do
     randfa="rand.fa"
@@ -21,7 +23,7 @@ for ref in DENV_1.fa HIV_1.fa; do
         python ./create_contigs.py $ref > $randfa;
         rm $outfa 2>/dev/null
         cmd="$JOINER -c $randfa -r $ref -o $outfa -v -v"
-        #echo "DEBUG Executing $cmd"
+        echo "DEBUG Executing $cmd" >> $log
         eval $cmd 2>>$log;
         # should only output one md5
         num_sqs=$(for f in $ref $outfa; do grep -v '^>' $f | tr -d '\n'  | $md5; done | sort -u | wc -l)
@@ -33,6 +35,5 @@ for ref in DENV_1.fa HIV_1.fa; do
         echo "$ref: $i/$NUM_TESTS passed" | tee -a $log
     done
 done
-echo "Done: all tests passed"
-#rm $log
+echo $SUCCESS_MSG | tee -a $log
 
